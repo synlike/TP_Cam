241fb8793eac2927c7fdbede3e25ef266c9b668e
David <synlike@gmail.com>
2021-01-27 15:22:41 +0100

    Exercice 4

--- Assets/Script/Maxence/FixedFollowView.cs
+++ Assets/Script/Maxence/FixedFollowView.cs
@@ -16,17 +16,50 @@ public class FixedFollowView : AView
     private float yaw;
     private float pitch;
 
-    private float yawOffsetMax;
-    private float pitchOffsetMax;
-    private GameObject centralPoint;
+    [Range(0f, 360f)]
+    public float yawOffsetMax;
+    [Range(0f, 90f)]
+    public float pitchOffsetMax;
 
+    public GameObject centralPoint;
+    private float centralPointYaw;
+    private float centralPointPitch;
 
-    void Awake()
+    private void Awake()
+    {
+        Vector3 centralPointEuler = centralPoint.transform.rotation.eulerAngles;
+        centralPointPitch = centralPointEuler.x;
+        centralPointYaw = centralPointEuler.y;
+    }
+
+    void Update()
+    {
+        CheckConstraints();
+    }
+
+    public void CheckConstraints()
     {
         Vector3 targetDir = (target.transform.position - transform.position).normalized;
 
         yaw = Mathf.Atan2(targetDir.x, targetDir.z) * Mathf.Rad2Deg;
         pitch = -Mathf.Asin(targetDir.y) * Mathf.Rad2Deg;
+
+        if (Mathf.Abs(yaw - centralPointYaw) > yawOffsetMax)
+        {
+            if (yaw < 0)
+                yaw = centralPointYaw - yawOffsetMax;
+            else
+                yaw = centralPointYaw + yawOffsetMax;
+        }
+
+        if (Mathf.Abs(pitch - centralPointPitch) > pitchOffsetMax)
+        {
+            if (pitch < 0)
+                pitch = centralPointPitch - pitchOffsetMax;
+            else
+                pitch = centralPointPitch + pitchOffsetMax;
+        }
+
     }
 
     public override CameraConfiguration GetConfiguration()
