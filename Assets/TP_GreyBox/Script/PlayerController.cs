74d51295d31f07b71251dc71b1129d0ca4957e12
David <synlike@gmail.com>
2021-01-28 09:14:43 +0100

    Import Greybox

---
+++ Assets/TP_GreyBox/Script/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 10.0f;
+
+	Rigidbody _rigidbody = null;
+	protected bool IsActive { get; private set; }
+
+	public void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+    {
+		Vector3 direction = Vector3.zero;
+		direction += Input.GetAxisRaw("Horizontal") * Vector3.right;
+		direction += Input.GetAxisRaw("Vertical") * Vector3.forward;
+		direction.Normalize();
+		_rigidbody.velocity = direction * speed + Vector3.up * _rigidbody.velocity.y;
+	}
+}
